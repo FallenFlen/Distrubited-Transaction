5a8e837f4338f59cb1db25748f76a9c39e913cdb
Fallenz <beijuspsn@163.com>
2023-09-24 20:37:23 +0800

    adjust module for storage

---
+++ storage/storage-main/src/main/resources/db/create_storage_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE storage
+(
+    id          varchar(32) primary key,
+    sku_id      varchar(32) not null,
+    `count`       bigint default 0,
+    create_time datetime(3) default CURRENT_TIMESTAMP (3),
+    create_by   varchar(50) not null,
+    update_time datetime(3) default CURRENT_TIMESTAMP (3),
+    update_by   varchar(50) not null,
+    unique index uq_sku_id(sku_id)
+)
