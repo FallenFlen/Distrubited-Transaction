32bdf06c42077ed29c40df84b529b2fbab8fb255
Fallenz <beijuspsn@163.com>
2023-10-04 17:19:32 +0800

    add common configs for finance and storage

---
+++ finance/finance-main/src/main/java/com/flz/dt/finance/common/config/interceptor/UserInterceptor.java
@@ -0,0 +1,30 @@
+package com.flz.dt.finance.common.config.interceptor;
+
+import com.flz.dt.common.constant.UserConstant;
+import com.flz.dt.common.context.User;
+import com.flz.dt.common.context.UserContext;
+import com.flz.dt.common.exception.UnAuthorizedException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+@Component
+public class UserInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String userId = Optional.ofNullable(request.getHeader(UserConstant.USER_ID_KEY))
+                .orElseThrow(() -> new UnAuthorizedException("Unauthorized!"));
+        String username = request.getHeader(UserConstant.USER_NAME_KEY);
+        UserContext.setUser(new User(userId, username));
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        UserContext.clear();
+    }
+}
---
+++ storage/storage-main/src/main/java/com/flz/dt/storage/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.flz.dt.storage.common.config;
+
+import com.flz.dt.order.common.config.interceptor.UserInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final UserInterceptor userInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userInterceptor).addPathPatterns("/**");
+    }
+}
