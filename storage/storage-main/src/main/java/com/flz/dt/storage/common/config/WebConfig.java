e230cec3cfd220973116df66ce570fe5a9628652
Fallenz <beijuspsn@163.com>
2023-10-04 17:23:20 +0800

    fix import

--- storage/storage-main/src/main/java/com/flz/dt/storage/common/config/WebConfig.java
+++ storage/storage-main/src/main/java/com/flz/dt/storage/common/config/WebConfig.java
@@ -1,6 +1,6 @@
 package com.flz.dt.storage.common.config;
 
-import com.flz.dt.order.common.config.interceptor.UserInterceptor;
+import com.flz.dt.storage.common.config.interceptor.UserInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
